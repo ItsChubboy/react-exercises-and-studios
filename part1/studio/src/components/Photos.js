dfb09c7b5f7ba556c2d7f7922934dfc2d4ae4a42
Colin Robinson <colin@Colins-Air.lan>
2023-08-03 20:01:22 -0500

    Finished recipe photo section of studio.

---
+++ part1/studio/src/components/Photos.js
@@ -0,0 +1,7 @@
+import styles from './Description.module.css';
+
+export default function RecipePhoto() {
+    return(
+        <img src="https://www.oliviascuisine.com/wp-content/uploads/2019/03/four-cheese-lasagna.jpg" alt="Four Cheese Lasagna Photo" className = {styles.imageUpdates} />
+    )
+}
